d1daa593732941af77cc20f753c5e9148cdcc9e3
MaxD <max.deroubaix@gmail.com>
2025-06-10 19:40:17 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/ArgentBank/' : '/',
   plugins: [react()],
-  base: '/argentbank/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 });
